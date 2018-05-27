a84ccdfcf9d642781f0924edfca333351429206b
luis.arevalos <luis.arevalos@my.jcu.edu.au>
2018-05-27 17:30:30 +1000

    login page created. update in css

---
+++ src/com/htransplant/servlet/loginServlet.java
@@ -0,0 +1,16 @@
+package com.htransplant.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/login")
+public class loginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/html/login.jsp").forward(req,resp);
+    }
+}
