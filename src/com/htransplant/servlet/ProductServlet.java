4bb532dc4b0529d868658c4764e76dbc06386a2c
luis.arevalos <luis.arevalos@my.jcu.edu.au>
2018-08-03 15:56:10 +1000

    new changes, css and enquiries

--- src/com/htransplant/servlet/ProductServlet.java
+++ src/com/htransplant/servlet/ProductServlet.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class ProductServlet extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		req.getRequestDispatcher("/html/index.html").forward(req, resp);
+		req.getRequestDispatcher("/index.html").forward(req, resp);
 	}
 
 }
