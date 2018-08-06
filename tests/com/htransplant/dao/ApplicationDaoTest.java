a567f0ad6df5b7494ff818c5adfa05415de48447
Tanmay <tanmayajit.thokal@my.jcu.edu.au>
2018-08-06 12:52:15 +1000

    Commit

---
+++ tests/com/htransplant/dao/ApplicationDaoTest.java
@@ -0,0 +1,26 @@
+package com.htransplant.dao;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ApplicationDaoTest {
+
+
+
+    @Test
+    public void listHairPackages() {
+    }
+
+    @Test
+    public void registerUser() {
+    }
+
+    @Test
+    public void validateUser() {
+    }
+
+    @Test
+    public void getProfileDetails() {
+    }
+}
---
+++ tests/com/htransplant/servlet/packagesServletTest.java
@@ -0,0 +1,12 @@
+package com.htransplant.servlet;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class packagesServletTest {
+
+    @Test
+    public void doGet() {
+    }
+}
